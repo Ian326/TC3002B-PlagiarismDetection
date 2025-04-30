bd297a4c5d34abdff31bdd2e34bdc32bd293933c
Ian326 <a01708940@tec.mx>
2025-04-29 20:26:23 -0600

    (chore): Update logic. Now receives 1 code

--- src/tools/AST.py
+++ src/tools/AST.py
@@ -1,6 +1,8 @@
 '''
-=================================================================================================
+==================================================================================================
 AST Parser
+==================================================================================================
+This file contains the code to parse the AST of a given code snippet using tree-sitter.
 ==================================================================================================
 Authors: 
   José Armando Rosas Balderas | A01704132
@@ -9,8 +11,6 @@ Authors:
 Date: 2025-Apr-28
 ==================================================================================================
 Libraries
-==================================================================================================
-This file contains the code to parse the AST of a given code snippet using tree-sitter.
 
 tree-stitter: Library for parsing source code into an AST.
 sys: Library for system-specific parameters and functions.
@@ -20,7 +20,7 @@ from tree_sitter import Language, Parser
 import sys
 
 # Define the constants for the Parser
-lang_grammar, path_grammar, file1, file2 = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+lang_grammar, path_grammar, file = sys.argv[1], sys.argv[2], sys.argv[3]
 
 # Initialize the parser
 parser = Parser()
@@ -45,15 +45,10 @@ else:
 
 
 # Read codes from the provided file paths
-with open(file1, 'rb') as f:
-    code1 = f.read()
-
-with open(file2, 'rb') as f:
-    code2 = f.read()
+with open(file, 'rb') as f:
+    code = f.read()
 
 # Generate the AST for the provided code snippets
-tree1 = parser.parse(code1)
-tree2 = parser.parse(code2)
+tree = parser.parse(code)
 
-print("Tree 1 root:", tree1.root_node)
-print("Tree 2 root:", tree2.root_node)
+print("Tree 1 root:", tree.root_node)
