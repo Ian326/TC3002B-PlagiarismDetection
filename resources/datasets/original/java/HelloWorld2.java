ee5b476cdd824fcdb31793806d00c2a9219e2fd0
Ian326 <a01708940@tec.mx>
2025-05-02 07:25:48 -0600

    (chore): Add Markov Chain & Aprox GED similarity

---
+++ resources/datasets/original/java/HelloWorld2.java
@@ -0,0 +1,26 @@
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        int k = 0;
+        if (k == 0) {
+            System.out.println("Esto no salee!");
+            if (k == 0) {
+            System.out.println("Esto no salee!");
+            } else {
+                System.out.println("asdasd12e!");
+                
+            }
+        } else {
+            System.out.println("asdasd12e!");
+            
+        }
+
+        for (int a = 0; a < 5; a++) {
+            System.out.println("Loop iteration: " + a);
+        }
+        while (k < 5) {
+            System.out.println("While loop iteration: " + k);
+            k++;
+        }
+
+    }
+}
