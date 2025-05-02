8361c3aaeefd24f8be483d3317fba5bbf6348dd6
Ian326 <a01708940@tec.mx>
2025-05-02 17:10:15 -0600

    (chore): Add test files

---
+++ resources/datasets/original/java/TestCFG.java
@@ -0,0 +1,11 @@
+public class TestCFG {
+    private static int suma(int x, int y){
+        return x + y;
+    }
+    public static void main(String[] args) {
+        int x= 5;
+        int y = 5;
+        System.out.println(suma(x, y));
+
+    }
+}
