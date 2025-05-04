ae87962077a4d192f97acad87d93a8245f78c32a
Ian326 <a01708940@tec.mx>
2025-05-04 05:54:35 -0600

    (feat): Improved logic for switch structures

--- resources/datasets/TestCFG.java
+++ resources/datasets/TestCFG.java
@@ -3,8 +3,21 @@ public class TestCFG {
         return a + b;
     }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        int result = sum(5, 10);
-        System.out.println("Sum: " + result);
+        // Lets make a switch statement
+        int x = 1;
+        switch (x) {
+            case 1:
+                System.out.println("x is 1");
+                System.out.println("Sum: " + sum(1, 2));
+                break;
+            case 2:
+                System.out.println("x is 2");
+                System.out.println("Sum: " + sum(2, 3));
+                break;
+            default:
+                System.out.println("x is not 1 or 2");
+                System.out.println("Sum: " + sum(0, 0));
+        }
+        int y = 2;
     }
 }
